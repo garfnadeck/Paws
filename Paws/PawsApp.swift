8ecee5e932d582988225ef0975a6e299828deba8
garfnadeck <serraopinto@gmail.com>
2025-09-11 17:10:15 -0300

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Thiago Serrão Braule Pinto on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
